96dc77dee3eadf356b8bfa3e58a5437c2a787ae0
leon <guerineauleon@gmail.com>
2022-11-17 01:24:04 +0100

    v1.23

--- Game.js
+++ Game.js
@@ -6,11 +6,231 @@ class Game
         numberOfColumns,
         objectif,
     ) {
+        this.players = players;
         this.numberOfRows = numberOfRows;
         this.numberOfColumns = numberOfColumns;
         this.objectif = objectif;
-        this.players = players;
 
         this.numberOfValidCells = 0;
+        this.gameBody = document.getElementById(this.constructor.name);
+        this.currentPlayer = this.players.find(()=>true);
+        this.HTMLTable = this.generateTable();
+        this.HTMLHeader = this.generateHeader();
+        this.cells = this.generateCells();
+        this.ended = false;
+        this.nextTurn()
+
+        this.modifyTableHeader(this.currentPlayer.name+' commence', 'lightgrey', this.currentPlayer.color);
+    }
+
+    /**
+     * Change le joueur du prochain tour
+     */
+    swapPlayers()
+    {
+        // on prends l'index du joueur dans la liste des joueurs
+        let currentPlayerIndex = this.players.indexOf(this.currentPlayer);
+        // si il y a un joueur suivant dans la liste des joueurs alors on l'attribu
+        if (this.players[currentPlayerIndex+1]) {
+            this.currentPlayer = this.players[currentPlayerIndex+1]
+        }
+        // sinon on prends le premier de la liste
+        else {
+            // find permets de trouver le premier element sans présumer que son index est 0
+            this.currentPlayer = this.players.find(()=>true)
+        }
+        // Modification du header
+        this.modifyTableHeader('Au tour de : '+this.currentPlayer.name, 'lightgrey', this.currentPlayer.color)
+    }
+
+    /**
+     * Génère la table HTML
+     *
+     * @returns {HTMLTableElement} L'élément HTML de la table
+     */
+    generateTable()
+    {
+        // génération de la table
+        let table = document.createElement('table');
+        // attribution de la table à la div du jeu sur la page principale
+        this.gameBody.appendChild(table);
+
+        return table
+    }
+
+    /**
+     * Génère le header de la table HTML
+     *
+     * @returns {HTMLTableElement} L'élément HTML du header
+     */
+    generateHeader()
+    {
+        // génération du header de la table
+        let tableHeader = this.HTMLTable.createTHead().insertRow().insertCell()
+        // colspan représente la largeur de la cellule en nombre de colones
+        tableHeader.colSpan = this.numberOfColumns
+
+        return tableHeader;
+    }
+
+    /**
+     * Génère les cellules
+     *
+     * @returns {Cell[]} La liste des cellules
+     */
+    generateCells()
+    {
+        // génération du corps de la table
+        let tableBody = this.HTMLTable.createTBody()
+        //génération des colones et des cellules
+        let cells = []
+        for(let row = 0; row<this.numberOfRows; row++) {
+            let tableRow = tableBody.insertRow()
+            for(let column = 0; column<this.numberOfColumns; column++) {
+                // génération de la cellule
+                let cell = new Cell(row, column, tableRow.insertCell())
+                this.numberOfValidCells++
+                // Attribution d'un nouvel objet Cellule dans la liste des cellules
+                cells.push(cell)
+                cell.HTMLCell.addEventListener('click', () => {
+                    this.play(cell)
+                    this.nextTurn()
+                })
+            }
+        }
+
+        return cells
+    }
+
+
+
+    nextTurn()
+    {
+        sleep(500).then( ()=> {
+            if (!this.currentPlayer.automatic || this.ended) {
+                return
+            }
+            this.currentPlayer.autoPlay(this)
+        })
+    }
+
+    /**
+     * Modifie le header du tableau
+     *
+     * @param {string}message Le message à afficher
+     * @param {string}backgroundColor La couleur du fond
+     * @param {string}fontColor La couleur du message
+     * @param {boolean}hasButton Est-ce qu'on affiche le boutton pour redémarrer la partie
+     */
+    modifyTableHeader(message, backgroundColor = 'lightgrey', fontColor = '', hasButton = false)
+    {
+        // modification du message et du fond du header
+        this.HTMLHeader.innerHTML = '<span style="color: '+fontColor+'">'+message+'</span>'
+        this.HTMLHeader.style.backgroundColor = backgroundColor
+        // affichage du boutton reset si demandé
+        if (hasButton) {
+            let restartButton = document.createElement('button')
+            restartButton.innerHTML = 'Nouvelle partie'
+            restartButton.addEventListener('click', () => {
+                restartGame(this)
+            })
+            this.HTMLHeader.appendChild(restartButton)
+        }
+    }
+
+    checkWin() {
+        this.checkWinHorizontal()
+        this.checkWinVertical()
+        this.checkWinDiagonalDown()
+        this.checkWinDiagonalUp()
+        if (this.numberOfValidCells <= 0) {
+            this.lose()
+        }
+    }
+
+    /**
+     * Vérifie pour chaque ligne si un joueur à gagner
+     */
+    checkWinHorizontal() {
+        for(let row = 0; row<this.numberOfRows; row++){
+            // filtre les cellules pour garder celles apartenant à une ligne précise
+            let cellsInRow = this.cells.filter(cell => {
+                return cell.row === row
+            })
+            // vérifie si un joueur à gagner
+            this.checkListForWin(cellsInRow)
+        }
+    }
+
+    checkWinVertical() {
+        for(let column = 0; column<this.numberOfColumns; column++){
+            let cellsinColumn = this.cells.filter(cell => {
+                return cell.column === column
+            })
+            this.checkListForWin(cellsinColumn)
+        }
+    }
+
+    checkWinDiagonalDown() {
+        for(let diagonal = -this.numberOfColumns; diagonal<this.numberOfRows; diagonal++){
+            let cellsinDiagonal = []
+            for(let column = 0; column<this.numberOfColumns; column++) {
+                let cell = this.cells.find( cell => {
+                    return cell.row === diagonal+column && cell.column === column
+                })
+                cell ? cellsinDiagonal.push(cell) : null
+
+            }
+            this.checkListForWin(cellsinDiagonal)
+        }
+    }
+
+    checkWinDiagonalUp() {
+        for(let diagonal = -this.numberOfColumns; diagonal<2*this.numberOfRows; diagonal++){
+            let cellsinDiagonal = []
+            for(let column = this.numberOfColumns; column>=0; column--) {
+                let cell = this.cells.find( cell => {
+                    return cell.row === diagonal-column && cell.column === column
+                })
+                cell ? cellsinDiagonal.push(cell) : null
+
+            }
+            this.checkListForWin(cellsinDiagonal)
+        }
+    }
+
+    /**
+     * prends une chaine de pions et regarde si un nombre suffisant de pions sont alignés pour qu'un joueur gagne
+     *
+     * @param cells
+     */
+    checkListForWin(cells) {
+        let counter = 1
+        let elem = null;
+        let winCells = []
+        cells.forEach((cell) => {
+            if (cell.player instanceof Player) {
+                if (cell.player === elem) {
+                    counter++
+                    winCells.push(cell)
+                } else {
+                    counter = 1;
+                    winCells = [cell]
+                }
+                if(counter === this.objectif) {
+                    winCells.forEach((cell) => {
+                        cell.HTMLCell.style.backgroundColor = 'lightgreen'
+                    })
+                    this.win(cell.player)
+                }
+            }
+            elem = cell.player
+        })
+    }
+
+    lose()
+    {
+        this.ended = true;
+        this.modifyTableHeader('Personne n\'as a gagner', 'indianred', '', true)
     }
 }
--- Puissance-4.js
+++ Puissance-4.js
@@ -1,113 +1,28 @@
-class Puissance4
+class Puissance4 extends Game
 {
     constructor(
         players=[],
         numberOfRows=6,
         numberOfColumns=7,
-        piecesToAlign=4
-    )
-    {
-        this.numberOfRows = numberOfRows;
-        this.numberOfColumns = numberOfColumns;
-        this.piecesToAlign = piecesToAlign;
-        this.players = players;
-        this.numberOfValidCells = 0;
+        objectif=4,
 
-        this.gameBody = document.getElementById('puissance-4')
-        this.currentPlayer = this.players.find(()=>true);
-        this.HTMLTable = this.generateTable();
-        this.HTMLHeader = this.generateHeader();
-        this.cells = this.generateCells();
-        this.ended = false;
-
-        this.modifyTableHeader(this.currentPlayer.name+' commence', 'lightgrey', this.currentPlayer.color);
-    }
-
-    /**
-     * Change le joueur du prochain tour
-     */
-    swapPlayers()
-    {
-        // on prends l'index du joueur dans la liste des joueurs
-        let currentPlayerIndex = this.players.indexOf(this.currentPlayer);
-        // si il y a un joueur suivant dans la liste des joueurs alors on l'attribu
-        if (this.players[currentPlayerIndex+1]) {
-            this.currentPlayer = this.players[currentPlayerIndex+1]
-        }
-        // sinon on prends le premier de la liste
-        else {
-            // find permets de trouver le premier element sans présumer que son index est 0
-            this.currentPlayer = this.players.find(()=>true)
-        }
-        // Modification du header
-        this.modifyTableHeader('Au tour de : '+this.currentPlayer.name, 'lightgrey', this.currentPlayer.color)
-    }
-
-    /**
-     * Génère la table HTML
-     *
-     * @returns {HTMLTableElement} L'élément HTML de la table
-     */
-    generateTable()
-    {
-        // génération de la table
-        let table = document.createElement('table');
-        // attribution de la table à la div du jeu sur la page principale
-        this.gameBody.appendChild(table);
-
-        return table
-    }
-
-    /**
-     * Génère le header de la table HTML
-     *
-     * @returns {HTMLTableElement} L'élément HTML du header
-     */
-    generateHeader()
-    {
-        // génération du header de la table
-        let tableHeader = this.HTMLTable.createTHead().insertRow().insertCell()
-        // colspan représente la largeur de la cellule en nombre de colones
-        tableHeader.colSpan = this.numberOfColumns
-
-        return tableHeader;
-    }
-
-    /**
-     * Génère les cellules
-     *
-     * @returns {Cell[]} La liste des cellules
-     */
-    generateCells()
-    {
-        // génération du corps de la table
-        let tableBody = this.HTMLTable.createTBody()
-        //génération des colones et des cellules
-        let cells = []
-        for(let row = 0; row<this.numberOfRows; row++) {
-            let tableRow = tableBody.insertRow()
-            for(let column = 0; column<this.numberOfColumns; column++) {
-                // génération de la cellule
-                let tableCell = tableRow.insertCell()
-                this.numberOfValidCells++
-                tableCell.addEventListener('click', () => {
-                    this.play(column)
-                })
-                // Attribution d'un nouvel objet Cellule dans la liste des cellules
-                cells.push(new Cell(row, column, tableCell))
-            }
-        }
-
-        return cells
+    ) {
+        super(
+            players,
+            numberOfRows,
+            numberOfColumns,
+            objectif,
+        );
     }
 
     /**
      * Pose le pion dans la colone sélectionnée et vérifie si la partie est gagnée
      *
-     * @param column La colone jouée
+     * @param cell La cellule jouée
      */
-    play(column)
+    play(cell)
     {
+        let column = cell.column
         //recherche les cases valides dans la column
         let validCellsInColumn = this.cells.filter(obj => {
             return obj.column === column && obj.valid === true;
@@ -126,131 +41,12 @@ class Puissance4
         this.checkWin()
     }
 
-    /**
-     * Modifie le header du tableau
-     *
-     * @param {string}message Le message à afficher
-     * @param {string}backgroundColor La couleur du fond
-     * @param {string}fontColor La couleur du message
-     * @param {boolean}hasButton Est-ce qu'on affiche le boutton pour redémarrer la partie
-     */
-    modifyTableHeader(message, backgroundColor = 'lightgrey', fontColor = '', hasButton = false)
-    {
-        // modification du message et du fond du header
-        this.HTMLHeader.innerHTML = '<span style="color: '+fontColor+'">'+message+'</span>'
-        this.HTMLHeader.style.backgroundColor = backgroundColor
-        // affichage du boutton reset si demandé
-        if (hasButton) {
-            let restartButton = document.createElement('button')
-            restartButton.innerHTML = 'Nouvelle partie'
-            restartButton.addEventListener('click', () => {
-                restartGame(this)
-            })
-            this.HTMLHeader.appendChild(restartButton)
-        }
-    }
-
-    checkWin() {
-        this.checkWinHorizontal()
-        this.checkWinVertical()
-        this.checkWinDiagonalDown()
-        this.checkWinDiagonalUp()
-        if (this.numberOfValidCells <= 0) {
-            this.lose()
-        }
-    }
-
-    /**
-     * Vérifie pour chaque ligne si un joueur à gagner
-     */
-    checkWinHorizontal() {
-        for(let row = 0; row<this.numberOfRows; row++){
-            // filtre les cellules pour garder celles apartenant à une ligne précise
-            let cellsInRow = this.cells.filter(cell => {
-                return cell.row === row
-            })
-            // vérifie si un joueur à gagner
-            this.checkListForWin(cellsInRow)
-        }
-    }
-
-    checkWinVertical() {
-        for(let column = 0; column<this.numberOfColumns; column++){
-            let cellsinColumn = this.cells.filter(cell => {
-                return cell.column === column
-            })
-            this.checkListForWin(cellsinColumn)
-        }
-    }
-
-    checkWinDiagonalDown() {
-        for(let diagonal = -this.numberOfColumns; diagonal<this.numberOfRows; diagonal++){
-            let cellsinDiagonal = []
-            for(let column = 0; column<this.numberOfColumns; column++) {
-                let cell = this.cells.find( cell => {
-                    return cell.row === diagonal+column && cell.column === column
-                })
-                cell ? cellsinDiagonal.push(cell) : null
-
-            }
-            this.checkListForWin(cellsinDiagonal)
-        }
-    }
-
-    checkWinDiagonalUp() {
-        for(let diagonal = -this.numberOfColumns; diagonal<2*this.numberOfRows; diagonal++){
-            let cellsinDiagonal = []
-            for(let column = this.numberOfColumns; column>=0; column--) {
-                let cell = this.cells.find( cell => {
-                    return cell.row === diagonal-column && cell.column === column
-                })
-                cell ? cellsinDiagonal.push(cell) : null
-
-            }
-            this.checkListForWin(cellsinDiagonal)
-        }
-    }
-
-    /**
-     * prends une chaine de pions et regarde si un nombre suffisant de pions sont alignés pour qu'un joueur gagne
-     *
-     * @param cells
-     */
-    checkListForWin(cells) {
-        let counter = 1
-        let elem = null;
-        let winCells = []
-        cells.forEach((cell) => {
-            if (cell.player instanceof Player) {
-                if (cell.player === elem) {
-                    counter++
-                    winCells.push(cell)
-                } else {
-                    counter = 1;
-                    winCells = [cell]
-                }
-                if(counter === this.piecesToAlign) {
-                    winCells.forEach((cell) => {
-                        cell.HTMLCell.style.backgroundColor = 'lightgreen'
-                    })
-                    this.win(cell.player)
-                }
-            }
-            elem = cell.player
-        })
-    }
-
+    //saute pas
     win(player)
     {
         this.ended = true;
         player.scorePuissance4++
         player.HTMLScorePuissance4Cell.innerHTML = player.scorePuissance4
         this.modifyTableHeader(player.name+' a gagner', 'lightgreen', '', true)
-    }
-
-    lose()
-    {
-        this.ended = true;
-        this.modifyTableHeader('Personne n\'as a gagner', 'lightred', '', true)
     }
 }
